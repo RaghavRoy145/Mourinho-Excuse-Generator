186580bd4e89ef9f6138c1ea2f6e076bd539adc2
RaghavRoy145 <46164465+RaghavRoy145@users.noreply.github.com>
2019-10-28 13:37:01 +0530

    new excuses added

--- www/script.js
+++ www/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    var excuse1 = ['The referee' , 'The 4th Official','The assistant referee','Their Manager','The media','The FA','The opposition','The weather','Paul Pogba','Luke Shaw', 'Anthony Martial'];
-    var excuse2 = ['did not listen to','underestimated the size of','turned a blind eye to','was disrespectful to','does not understand','has destablished','has provoked divisions in','plays a different system to','was intimidated by','ignored'];
-    var excuse3 = ['my ego.','the cosmic principle of Karma.','my Special One talent.','my innate superiority.','my instructions.','the dressing room.','all the trophies I have won.','my signings.',"Paul Pogba's transfer fee.","Sir Alex Ferguson."];
+    var excuse1 = ['The referee' , 'The 4th Official','The assistant referee','Their Manager','The media','The FA','The opposition','The weather','Paul Pogba','Luke Shaw', 'Anthony Martial','my pregnant dad','my ugly fish'];
+    var excuse2 = ['did not listen to','underestimated the size of','turned a blind eye to','was disrespectful to','does not understand','has destablished','has provoked divisions in','plays a different system to','was intimidated by','ignored',"likes beastiality because","died of std because"];
+    var excuse3 = ['my ego.','the cosmic principle of Karma.','my Special One talent.','my innate superiority.','my instructions.','the dressing room.','all the trophies I have won.','my signings.',"Paul Pogba's transfer fee.","Sir Alex Ferguson.","of my smoking habit","of footballs"];
     var random1 = excuse1[Math.floor(Math.random() * excuse1.length)];
     var random2 = excuse2[Math.floor(Math.random() * excuse2.length)];
     var random3 = excuse3[Math.floor(Math.random() * excuse3.length)];
